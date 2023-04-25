a7831c8c805c2565363dce25f8105472c836be03
PavolSloboda <pavol.sloboda02@gmail.com>
2023-04-25 23:41:21 +0200

    opravena funkcia odmocnina c Namespaces.h

--- Namespaces.h
+++ Namespaces.h
@@ -59,38 +59,7 @@ namespace mathematic {
 	*/
 	inline double MySqrt(double number, double precision)
 	{
-		double start = 0, end = number;
-		double mid;
-
-		double ans  = 0;
-
-		while (start <= end) {
-			mid = (start + end) / 2;
-			if (mid * mid == number) {
-				ans = mid;
-				break;
-			}
-
-			if (mid * mid < number) {
-				start = mid + 1;
-				ans = mid;
-			}
-
-			else {
-				end = mid - 1;
-			}
-		}
-
-		float increment = 0.1;
-		for (int i = 0; i < precision; i++) {
-			while (ans * ans <= number) {
-				ans += increment;
-			}
-
-			ans = ans - increment;
-			increment = increment / 10;
-		}
-		return ans;
+		return pow(number,(1/precision));
 	}
 
 	/** @brief Vypocet faktorialu
